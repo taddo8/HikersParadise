3553ac5a4d663b10cd68cb5f24af60ae4632c0dc
taddo8 <taddo8@yahoo.com>
2021-06-15 23:33:11 -0500

    Add project files.

---
+++ HikersParadise/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(HikersParadise.Startup))]
+namespace HikersParadise
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
